34b0de82daa2f969a4c9ea69c457c92dcf542453
Ian Miller <theiandmiller@gmail.com>
2022-01-19 06:49:41 -0600

    Use Frequency Counter to find Anagrams

---
+++ Problem Solving Patterns/anagram.js
@@ -0,0 +1,12 @@
+/* Given two strings, write a function to determine
+if the second string is an anagram of the first string. */
+
+/* 
+isAnagram('', '') // true
+isAnagram('aaz', 'zza') // false
+isAnagram('anagram', 'nagaram') // true
+isAnagram('rat', 'car') // false
+isAnagram('awesome', 'awesom') // false
+isAnagram('qwerty', 'qeywrt') // true
+isAnagram('texttwisttime', 'timetwisttext') // true
+*/
