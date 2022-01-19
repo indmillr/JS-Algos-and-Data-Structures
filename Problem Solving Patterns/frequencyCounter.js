a51973f8e37e71f2c42ad4521fee48c8477282c6
Ian Miller <theiandmiller@gmail.com>
2022-01-19 06:44:54 -0600

    Refactored Frequency Counter

--- Problem Solving Patterns/frequencyCounter.js
+++ Problem Solving Patterns/frequencyCounter.js
@@ -12,8 +12,8 @@ same([1, 2, 3], [1, 9]) // false
 same([1, 2, 3], [4, 4, 1]) // false (must be same frequency)
 */
 
-// NESTED LOOP SOLUTION
-function same(arr1, arr2) {
+// NESTED LOOP SOLUTION (naive approach)
+function sameNaive(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -23,6 +23,30 @@ function same(arr1, arr2) {
       return false;
     }
     arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+// FREQUENCY COUNTER SOLUTION
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = frequencyCounter2[val] || 0 + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
   }
   return true;
 }
