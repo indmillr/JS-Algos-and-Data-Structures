c71552a7ae8660bc4487ef4588e346e1f7215198
Ian Miller <theiandmiller@gmail.com>
2022-01-20 05:22:55 -0600

    Using 'Frequency Counter Pattern' to determine if two given strings are anagrams

--- Problem Solving Patterns/anagram.js
+++ Problem Solving Patterns/anagram.js
@@ -10,3 +10,26 @@ isAnagram('awesome', 'awesom') // false
 isAnagram('qwerty', 'qeywrt') // true
 isAnagram('texttwisttime', 'timetwisttext') // true
 */
+function validAnagram(arr1, arr2) {
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  let freq1 = {};
+  let freq2 = {};
+
+  for (let val of arr1) {
+    freq1[val] = (freq1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    freq2[val] = (freq2[val] || 0) + 1;
+  }
+  for (let key in freq1) {
+    if (!(key in freq2)) {
+      return false;
+    }
+    if (freq2[key] !== freq1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
