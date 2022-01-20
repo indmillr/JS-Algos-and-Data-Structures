bcab1d273dfe1fa058c4c9fa8f60b765c495ad86
Ian Miller <theiandmiller@gmail.com>
2022-01-20 06:53:12 -0600

    Multiple Pointers Pattern with examples

--- Problem Solving Patterns/patternMultiplePointers.js
+++ Problem Solving Patterns/patternMultiplePointers.js
@@ -25,7 +25,7 @@ function sumZero(arr) {
   }
 }
 
-// MULTIPLE POINTERS SOLUTION
+// MULTIPLE POINTERS SOLUTION (starting at Left and Right, moving inwards)
 function sumZero(arr) {
   let left = 0;
   let right = arr.length - 1;
@@ -39,4 +39,31 @@ function sumZero(arr) {
       left++;
     }
   }
+}
+
+/*
+Implement a function called countUniqueValues, which accepts a SORTED array,
+and counts the unique values in the array. There can be negative numbers in
+the array, but it will always be sorted.
+*/
+
+/*
+countUniqueValues([1, 1, 1, 1, 1, 2]) // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-2, -1, -1, 0, 1]) // 4
+*/
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let left = 0;
+  for (let right = 1; right < arr.length; right++) {
+    if (arr[left] !== arr[right]) {
+      left++;
+      arr[left] = arr[right];
+    }
+  }
+  return left + 1;
 }
