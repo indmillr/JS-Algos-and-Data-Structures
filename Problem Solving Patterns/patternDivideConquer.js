0a92cade4a6458f4fc72c36706076d8fdb75ef3a
Ian Miller <theiandmiller@gmail.com>
2022-01-25 06:10:24 -0600

    Divide and Conquer pattern

--- Problem Solving Patterns/patternDivideConquer.js
+++ Problem Solving Patterns/patternDivideConquer.js
@@ -16,3 +16,33 @@ search([1, 2, 3, 4, 5, 6], 4) // 3
 search([1, 2, 3, 4, 5, 6], 6) // 5
 search([1, 2, 3, 4, 5, 6], 11) // -1
 */
+
+// NESTED LOOP SOLUTION (naive)
+function search(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// DIVIDE AND CONQUER SOLUTION (This is Binary Search!)
+function search(arr, val) {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = array[middle];
+
+    if (arr[middle] < val) {
+      min = middle + 1;
+    } else if (arr[middle] > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
