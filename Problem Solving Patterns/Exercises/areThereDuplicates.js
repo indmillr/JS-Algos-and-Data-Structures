e233f46d8e6b933c4878cf7f4f4768a6a2c77cde
Ian Miller <theiandmiller@gmail.com>
2022-01-28 05:48:49 -0600

    outline Multiple Pointers pattern exercise

---
+++ Problem Solving Patterns/Exercises/areThereDuplicates.js
@@ -0,0 +1,15 @@
+/*
+Implement a function called areThereDuplicates which accepts a
+variable number of args and checks whether there are any duplicates
+among the args passed in. Solve this using Multiple Pointers pattern.
+
+RESTRICTIONS:
+Time Complexity must be 0(n)
+Space Complexity must be 0(n)
+*/
+
+/*
+areThereDuplicates(1, 2, 3) // false;
+areThereDuplicates(1, 2, 2) // true;
+areThereDuplicates('a', 'b', 'c', 'a') // true
+*/
