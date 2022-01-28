20c5bd0a14f2d824d5856297bd59d9b5022cdd4d
Ian Miller <theiandmiller@gmail.com>
2022-01-28 05:38:20 -0600

    Frequency Counter exercise first attempt

--- Problem Solving Patterns/Exercises/frequencyCounter.js
+++ Problem Solving Patterns/Exercises/frequencyCounter.js
@@ -12,4 +12,27 @@ sameFrequency(3589578, 5879385) // true
 sameFrequency(22, 222) // false
 */
 
-function sameFrequency(num1, num2) {}
+function sameFrequency(num1, num2) {
+  let arr1 = Array.from(String(num1), Number);
+  let arr2 = Array.from(String(num2), Number);
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let counter1 = {};
+  let counter2 = {};
+  for (let val of arr1) {
+    counter1[val] = (counter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    counter2[val] = (counter2[val] || 0) + 1;
+  }
+  for (let key in counter1) {
+    if (!(key in counter2)) {
+      return false;
+    }
+    if (counter2[key] !== counter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
