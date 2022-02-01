8b773db4a0d24633fee791f413ddbaa078797e29
Ian Miller <theiandmiller@gmail.com>
2022-01-31 19:41:43 -0600

    setup

---
+++ Problem Solving Patterns/Exercises/isSubsequence.js
@@ -0,0 +1,15 @@
+/*
+Write a function called isSubsequence which takes two strings
+and checks whether the characters in the first string form
+a subsequence of the characters in the second string.
+In other words, the function should check whether the characters
+in the first string appear somewhere in the second string,
+WITHOUT THEIR ORDER CHANGING.
+*/
+
+/*
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false
+*/
