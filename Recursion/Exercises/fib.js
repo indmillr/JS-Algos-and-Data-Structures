497755548fed613c6de5fcf1148f070d7bb4e1f8
Ian Miller <theiandmiller@gmail.com>
2022-02-01 06:35:09 -0600

    Fibonacci sequence Recursion Exercise

---
+++ Recursion/Exercises/fib.js
@@ -0,0 +1,11 @@
+/*
+Write a function called fib that accepts a number and
+returns the nth number in the Fibonacci sequence.
+*/
+
+function fib(num) {
+  if (num <= 2) {
+    return 1;
+  }
+  return fib(num - 1) + fib(num - 2);
+}
