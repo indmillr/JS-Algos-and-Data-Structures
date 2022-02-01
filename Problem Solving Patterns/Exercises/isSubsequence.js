4ebf35bcbe0d1242e275ade1c5e85f8d452597f8
Ian Miller <theiandmiller@gmail.com>
2022-01-31 19:51:58 -0600

    isSubsequence Exercise attempt 1

--- Problem Solving Patterns/Exercises/isSubsequence.js
+++ Problem Solving Patterns/Exercises/isSubsequence.js
@@ -13,3 +13,21 @@ isSubsequence('sing', 'sting'); // true
 isSubsequence('abc', 'abracadabra'); // true
 isSubsequence('abc', 'acb'); // false
 */
+
+function isSubsequence(str1, str2) {
+  let pointer1 = 0;
+  let pointer2 = 0;
+  if (!str1) {
+    return true;
+  }
+  while (pointer2 < str2.length) {
+    if (str2[pointer2] === str1[pointer1]) {
+      pointer1++;
+    }
+    if (pointer1 === str1.length) {
+      return true;
+    }
+    pointer2++;
+  }
+  return false;
+}
