61a499c839d4c77133d084d7b4d33b507581708c
Ian Miller <theiandmiller@gmail.com>
2022-01-31 20:05:14 -0600

    setup minSubArrayLen Exercise

---
+++ Problem Solving Patterns/Exercises/minSubArrayLen.js
@@ -0,0 +1,7 @@
+/*
+Write a function called minSubArrayLen which accepts two paramaters:
+an array of positive integers and a positive integer.
+Return the minimal length of a contiguous subarray of which the sum
+is greater than or equal to the integer passed to the function.
+If there isn't one, return 0 instead.
+*/
