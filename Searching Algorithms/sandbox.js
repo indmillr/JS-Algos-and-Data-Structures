529b978cbbb40bb0dc88ffba4d205cfe5ba2e6aa
Ian Miller <theiandmiller@gmail.com>
2022-02-07 06:56:39 -0600

    Begin 'Searching Algorithms' study

---
+++ Searching Algorithms/sandbox.js
@@ -0,0 +1,19 @@
+/*
+Implement linear search on arrays.
+Implement binary search on sorted arrays.
+Implement a naive string searching algorithm.
+Implement the KMP string searching algorithm.
+*/
+
+// LINEAR SEARCH
+// Look at every single item in the array, in order, at a set interval.
+function linearSearch(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      // return the index at which the item is found
+      return i;
+    }
+  }
+  // return -1 if the item is NOT found
+  return -1;
+}
